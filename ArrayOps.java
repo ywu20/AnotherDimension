f4bc43e8da16fe44c42e6cabcd493c24d353c417
Yuqing Wu <ywu20@stuy.edu>
2020-10-23 13:20:04 -0400

    sum()

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,4 +1,5 @@
 public class ArrayOps{
+
   public static int sum(int[] arr){
     int sum=0;
     for (int i=0;i<arr.length;i++){
@@ -6,8 +7,17 @@ public class ArrayOps{
     }
     return sum;
   }
+
+  public static int largest(int[] arr){
+    int largest=arr[0];
+    for(int i=1;i<arr.length;i++){
+      largest=Math.max(largest, arr[i]);
+    }
+    return largest;
+  }
   public static void main(String[] args){
     int[] a={4,53,8,2};
-    System.out.println(sum(a));
+    //System.out.println(sum(a));
+    System.out.println(largest(a));
   }
 }
